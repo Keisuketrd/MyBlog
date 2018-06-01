e658f43f0eb2a91e1db3b92bf7ba0abf58945f41
Keisuke Terada <yxjkf818@yahoo.co.jp>
2018-06-01 09:17:25 +0000

    Add name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_31_031804) do
+ActiveRecord::Schema.define(version: 2018_06_01_085025) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
